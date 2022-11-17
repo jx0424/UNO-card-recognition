9beae1fafcb6d786cdb2ab2d434f7ed69c21941d
jx0424 <97114405+jx0424@users.noreply.github.com>
2022-11-17 14:42:23 +0000

    Add files via upload

---
+++ Image reading.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+#function to read image files from computer
+#image_read(user_choice):
+    #card = user choice
+user_choice = input("Please enter the card of choice you want the program to identify: ")
+print(user_choice)
+
+#image = cv2.imread(r'C:\Users\brend\OneDrive - Middlesex University\Desktop\images\b3.jpg')
+#cv2.imshow('picture',image)
+                    
+    
