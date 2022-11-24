f408ef16655f937f8a88447287d60dbd64982fbc
jx0424 <jl1280@live.mdx.ac.uk>
2022-11-24 09:21:48 +0000

    morphing,biinarisation and contours

--- Image reading.py
+++ Image reading.py
@@ -1,34 +1,54 @@
 import cv2
 import numpy as np
 
-#function to read image files from computer
-#image_read(user_choice):
-    #card = user choice
-##uno_cards = [b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bR,bS,bT,,g0,g1,g2,g3,g4,g5,g6,g7,g8,g9,gR,gS,gT,]
-##print("Choice of UNO card:
-##      
-##user_choice = input("Please enter the card of choice you want the program to identify: ")
-##print(user_choice)
-##
-#image = cv2.imread(r'C:\Users\brend\OneDrive - Middlesex University\Desktop\images\b3.jpg')
-#cv2.imshow('picture',image)
-##                    
-##    
+#Load in image from folder 
+image = cv2.imread(r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\b0.jpg')
+bilateral_image = cv2.bilateralFilter(image, 9 , 50, 50)
+cv2.imshow('Bilateral image', bilateral_image)
+blur_image = cv2.GaussianBlur(bilateral_image, (9,9), cv2.BORDER_CONSTANT)
+cv2.imshow('blur',blur_image)
+BW_image = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Black and White', BW_image)
+threshed_image = cv2.adaptiveThreshold(BW_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9 ,7)
+cv2.imshow('threshed image' ,threshed_image)
+# canny_image = cv2.Canny (threshed_image, 100, 100)
+# cv2.imshow('canny', canny_image)
+contours, hierarchy = cv2.findContours(threshed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+print(len(contours))
 
-#function to read image file from computer
-path = r'C:\Users\brend\OneDrive - Middlesex University\Desktop\images\b2.jpg'
-image = cv2.imread(path ,0) #read image file in grayscale from folder through the assigned path
-#blurring the image to decrease noise
-blur_image = cv2.GaussianBlur(image, (15,15), cv2.BORDER_DEFAULT)
+cv2.drawContours(image, contours, -1 , (0,255,0), 1)
+cv2.imshow('contour image', image)
 
-thresh_image = cv2.adaptiveThreshold(blur_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 35, 2) # compute a threshold value for each pixel, based on neighbourhood
 
-canny_image = cv2.Canny(thresh_image, 125, 175)
 
-dilated_image = cv2.dilate(canny_image, (7,7), iterations=3)
 
-#eroded_image = cv2.erode(dilated_image, (3,3), iterations= 1)
 
-cv2.imshow ('UNO Card', eroded_image) #display the image
+
+
 cv2.waitKey(0)
+
+
+# #function to read image file from computer
+# path = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\b1.jpg'
+# image = cv2.imread(path) #read image file in grayscale from folder through the assigned path
+# #blurring the image to decrease noise
+# #blur_image = cv2.GaussianBlur(image, (15,15), cv2.BORDER_DEFAULT)
+# # blank_image = np.zeros(image.shape, dtype = 'uint8')
+# # thresh_image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 35, 2) # compute a threshold value for each pixel, based on neighbourhood
+
+# # canny_image = cv2.Canny(thresh_image, 125, 175)
+
+# # #dilated_image = cv2.dilate(canny_image, (7,7), iterations=3)
+# #bilateral = cv2.bilaterralFilter(img, 10 , 35, 25)
+# # contour , hierachies = cv2.findContours(thresh_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# # print(len(contour))
+
+# rgb = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+# cv2.imshow('rgb', rgb)
+# # cv2.drawContours(blank_image, contour, -1, (0,0,255), 2)
+# # cv2.imshow('Contours drawn', blank_image)
+# #eroded_image = cv2.erode(dilated_image, (3,3), iterations= 1)
+
+# cv2.imshow ('UNO Card', image) #display the image
+# cv2.waitKey(0)
  
