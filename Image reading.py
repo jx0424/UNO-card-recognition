3710e4654dac3a85b349617d3f6186767644436e
jx0424 <jl1280@live.mdx.ac.uk>
2022-12-05 00:50:54 +0000

    Contour fix
    Implemented fix to all colour contour except wild cards

--- Image reading.py
+++ Image reading.py
@@ -2,58 +2,125 @@ import cv2
 import numpy as np
 
 #Load in image from folder 
-image = cv2.imread(r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\b0.jpg')
-bilateral_image = cv2.bilateralFilter(image, 9 , 100, 100)
-cv2.imshow('Bilateral image', bilateral_image)
-blur_image = cv2.GaussianBlur(bilateral_image, (3,3), cv2.BORDER_CONSTANT)
-cv2.imshow('blur',blur_image)
-BW_image = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Black and White', BW_image)
-thr_value, threshed_image = cv2.threshold(BW_image, 180, 255, cv2.THRESH_BINARY)
-cv2.imshow('threshed image' ,threshed_image)
+image = cv2.imread(r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\y1.jpg')
 
-kernel = np.ones((9,9), np.uint8)
-image_close = cv2.morphologyEx(threshed_image, cv2.MORPH_OPEN, kernel)      # morphology correction
-img_canny = cv2.Canny(image_close, 50, 100) 
+try:
+    bilateral_image = cv2.bilateralFilter(image, 9 , 100, 100)
+    cv2.imshow('Bilateral image', bilateral_image)
+    blur_image = cv2.GaussianBlur(bilateral_image, (3,3), cv2.BORDER_CONSTANT)
+    cv2.imshow('blur',blur_image)
+    BW_image = cv2.cvtColor(blur_image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Black and White', BW_image)
+    thr_value, threshed_image = cv2.threshold(BW_image, 165, 255, cv2.THRESH_BINARY)
+    cv2.imshow('threshed image' ,threshed_image)
+    kernel = np.ones((5,5), np.uint8)
+    image_close = cv2.morphologyEx(threshed_image, cv2.MORPH_OPEN, kernel)      # morphology correction
+    image_open = cv2.morphologyEx(image_close, cv2.MORPH_CLOSE, kernel)
+    #img_canny = cv2.Canny(image_close, 50, 100) 
 
-# canny_image = cv2.Canny (threshed_image, 100, 100)
-# cv2.imshow('canny', canny_image)
-contours, hierarchy = cv2.findContours(image_close, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-print(len(contours))
+    # canny_image = cv2.Canny (threshed_image, 100, 100)
+    #cv2.imshow('canny', img_canny)
+    contours, hierarchy = cv2.findContours(image_close, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
 
-cv2.drawContours(image, contours, -1 , (0,255,0), 2)
-cv2.imshow('contour image', image)
+    cv2.drawContours(image, contours, -1 , (0,255,0), 2)
+    cv2.imshow('contour image', image)
 
+#cv2.waitKey(0)
+    for i, c in enumerate(contours):         # loop through all the found contours
+                    print(i, ':', hierarchy[0, i])          # display contour hierarchy
+                    #print('length: ', len(c))               # display numbr of points in contour c
+                    perimeter = cv2.arcLength(c, True)     # perimeter of contour c (curved length)
+                    print('perimeter: ', perimeter)               
+                    epsilon = 0.006*perimeter    # parameter of polygon approximation: smaller values provide more vertices
+                    vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
+                    print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+                    ellipse = cv2.fitEllipse(c)
+                    (xc,yc),(d1,d2), angle = ellipse
+                    axes_ratio = round (d1/d2,  3)
+                    print('axes ratio', axes_ratio, '\n')
+                    cv2.drawContours(image, [c], 0, (0, 255, 0), 3)   # paint contour c
+                    cv2.putText(image, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c    
+                    [x,y,w,h] = cv2.boundingRect(c)
+                    cv2.rectangle(image, (x,y), (x+w,y+h), (255, 0, 0), 2)
+                    #cv2.ellipse(image, ellipse, (0, 0, 255), 1)
+                    #cropped = image[250:410,260:395]
+                    #cv2.imshow('cropped', cropped) 
+                    cv2.imshow('contours',image)
+                    sample = [perimeter, vertex_approx, axes_ratio]
+except: 
+    #bilateral_image = cv2.bilateralFilter(image, 9 , 100, 100)
+    #cv2.imshow('Bilateral image', bilateral_image)
+    BW_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur_image = cv2.blur(BW_image ,(5,5))
+    cv2.imshow('blur',blur_image)
+   
+    cv2.imshow('Black and White', BW_image)
+    th, threshed_image = cv2.threshold(blur_image,140,255,cv2.THRESH_BINARY_INV)
+    cv2.imshow('threshed image' ,threshed_image)
+    kernel = np.ones((5,5), np.uint8)
+    #image_close = cv2.morphologyEx(threshed_image, cv2.MORPH_OPEN, kernel)      # morphology correction
+    #image_open = cv2.morphologyEx(image_close, cv2.MORPH_CLOSE, kernel)
+    #img_canny = cv2.Canny(image_close, 50, 100) 
 
+    # canny_image = cv2.Canny (threshed_image, 100, 100)
+    #cv2.imshow('canny', img_canny)
+    contours, hierarchy = cv2.findContours(threshed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
 
+    cv2.drawContours(image, contours, -1 , (0,255,0), 2)
+    cv2.imshow('contour image', image)
 
-
-
-
+#cv2.waitKey(0)
+    # for i, c in enumerate(contours):         # loop through all the found contours
+    #                 print(i, ':', hierarchy[0, i])          # display contour hierarchy
+    #                 #print('length: ', len(c))               # display numbr of points in contour c
+    #                 perimeter = cv2.arcLength(c, True)     # perimeter of contour c (curved length)
+    #                 print('perimeter: ', perimeter)               
+    #                 epsilon = 0.006*perimeter    # parameter of polygon approximation: smaller values provide more vertices
+    #                 vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
+    #                 print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+    #                 ellipse = cv2.fitEllipse(c)
+    #                 (xc,yc),(d1,d2), angle = ellipse
+    #                 axes_ratio = round (d1/d2,  3)
+    #                 print('axes ratio', axes_ratio, '\n')
+    #                 cv2.drawContours(image, [c], 0, (0, 255, 0), 3)   # paint contour c
+    #                 cv2.putText(image, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c    
+    #                 [x,y,w,h] = cv2.boundingRect(c)
+    #                 cv2.rectangle(image, (x,y), (x+w,y+h), (255, 0, 0), 2)
+    #                 #cv2.ellipse(image, ellipse, (0, 0, 255), 1)
+    #                 #cropped = image[250:410,260:395]
+    #                 #cv2.imshow('cropped', cropped) 
+    #                 cv2.imshow('contours',image)
+    #                 sample = [perimeter, vertex_approx, axes_ratio]
+#     #other image processing for other colour
+#         # for i, c in enumerate(contours):         # loop through all the found contours
+#         #             print(i, ':', hierarchy[0, i])          # display contour hierarchy
+#         #             #print('length: ', len(c))               # display numbr of points in contour c
+#         #             perimeter = cv2.arcLength(c, True)     # perimeter of contour c (curved length)
+#         #             print('perimeter: ', perimeter)               
+#         #             epsilon = 0.006*perimeter    # parameter of polygon approximation: smaller values provide more vertices
+#         #             vertex_approx = len(cv2.approxPolyDP(c, epsilon, True))     # approximate with polygon
+#         #             print('approx corners: ', vertex_approx, '\n')                    # number of vertices
+#         #             ellipse = cv2.fitEllipse(c)
+#         #             (xc,yc),(d1,d2), angle = ellipse
+#         #             axes_ratio = round (d1/d2,  3)
+#         #             print('axes ratio', axes_ratio, '\n')
+#         #             cv2.drawContours(image, [c], 0, (0, 255, 0), 3)   # paint contour c
+#         #             cv2.putText(image, str(i), (c[0, 0, 0]+20, c[0, 0, 1]+30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))    # identify contour c    
+#         #             [x,y,w,h] = cv2.boundingRect(c)
+#         #             cv2.rectangle(image, (x,y), (x+w,y+h), (255, 0, 0), 2)
+#         #             #cv2.ellipse(image, ellipse, (0, 0, 255), 1)
+#         #             #cropped = image[250:410,260:395]
+#         #             #cv2.imshow('cropped', cropped) 
+#         #             cv2.imshow('contours',image)
+#         #             sample = [perimeter, vertex_approx, axes_ratio]
+    
 cv2.waitKey(0)
 
+#if perimeter > asdasdas and 
 
-# #function to read image file from computer
-# path = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\b1.jpg'
-# image = cv2.imread(path) #read image file in grayscale from folder through the assigned path
-# #blurring the image to decrease noise
-# #blur_image = cv2.GaussianBlur(image, (15,15), cv2.BORDER_DEFAULT)
-# # blank_image = np.zeros(image.shape, dtype = 'uint8')
-# # thresh_image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 35, 2) # compute a threshold value for each pixel, based on neighbourhood
 
-# # canny_image = cv2.Canny(thresh_image, 125, 175)
 
-# # #dilated_image = cv2.dilate(canny_image, (7,7), iterations=3)
-# #bilateral = cv2.bilaterralFilter(img, 10 , 35, 25)
-# # contour , hierachies = cv2.findContours(thresh_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-# # print(len(contour))
 
-# rgb = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-# cv2.imshow('rgb', rgb)
-# # cv2.drawContours(blank_image, contour, -1, (0,0,255), 2)
-# # cv2.imshow('Contours drawn', blank_image)
-# #eroded_image = cv2.erode(dilated_image, (3,3), iterations= 1)
 
-# cv2.imshow ('UNO Card', image) #display the image
-# cv2.waitKey(0)
- 
--- test.py
+++ test.py
@@ -6,19 +6,21 @@ from os import listdir
 userchoice = 'blue'
 data_X = []
 if userchoice == 'blue':
-    folder_dir = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\B'
+    folder_dir = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images'
     for images in os.listdir(folder_dir):
     # check if the image ends with jpg  
         if images.endswith(".jpg"):
             #path = r'C:\Users\brend\OneDrive - Middlesex University\Documents\GitHub\UNO-card-recognition\images\b0.jpg'
             path = os.path.join(folder_dir, images)
             img_colour = cv2.imread(path)   # open the saved image in colour
+            #try
             img = cv2.cvtColor(img_colour, cv2.COLOR_BGR2GRAY)   # convert to B/W
-            img_sm = cv2.blur(img, (9,9))         # smoothing
-            thr_value, img_th = cv2.threshold(img_sm, 150, 255, cv2.THRESH_BINARY)   # binarisation
-            kernel = np.ones((3,3), np.uint8)
-            img_close = cv2.morphologyEx(img_th, cv2.MORPH_OPEN, kernel)      # morphology correction
-            img_canny = cv2.Canny(img_close, 100, 200)                          # edge detection
+            img_sm = cv2.blur(img, (7,7))         # smoothing
+            thr_value, img_th = cv2.threshold(img_sm, 160, 255, cv2.THRESH_BINARY_inv)   # binarisation
+            kernel = np.ones((5,5), np.uint8)
+            img_canny = cv2.Canny(img_th, 50, 100)  
+            img_close = cv2.morphologyEx(img_canny, cv2.MORPH_CLOSE, kernel)      # morphology correction
+                                    # edge detection
             contours, hierarchy = cv2.findContours(img_close, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   # extract contours on binarised image, not on canny
         # #cv2.drawContours(img_colour, contours, -1, (0, 255, 0), 1)         # paint contours on top of original coloured mage
     # #cv2.imshow('picture', img_colour)
@@ -44,9 +46,9 @@ if userchoice == 'blue':
                 cv2.imshow('contours',img_colour)
                 sample = [perimeter, vertex_approx, axes_ratio]
                 #print (hierarchy)
-                if hierarchy[i][0] == -1 and hierarchy[i][3]== 0:
+                #if hierarchy[i][0] == -1 and hierarchy[i][3]== 0:
                 #     print(i)
-                    data_X.append(sample)
+                #    data_X.append(sample)
                 #     print('hello')
                         # if cols[0] == -1 and cols [3]==0:
                         #     data_X.append(sample)
@@ -68,4 +70,4 @@ print(data_X)
 
 # Data_X_sample(sample)
 
-#Can crop image after countour is plotted 
+#Can crop image after countour is plotted              
